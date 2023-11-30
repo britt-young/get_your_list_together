21cbfdb8f8745380127b1f5199e2e25e303f80e9
Maggie McCausland <maggiemccausland@hotmail.com>
2023-11-30 15:03:48 -0700

    removed server.js commented out code, server successfully hosting with sequelizer but not getting routes

--- server.js
+++ server.js
@@ -14,27 +14,6 @@ const PORT = process.env.PORT || 3000;
 // Set Handlebars as the default templating engine
 const hbs = exphbs.create({ helpers });
 
-// app.engine('handlebars', exphbs());
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-
-//Define routes and render a handlebars template
-// app.get('/', (req, res) => {
-//     const data = {
-// //add object content here to create template for key:value pairs
-// // e.g name: 'John Doe',
-//     }
-//     res.render('home', data);
-// });
-
-// Start the Express server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Server listening on port ${PORT}`);
-// });
-
-
-
 // Configure and link a session object with the sequelize store
 const sess = {
     secret: "secret",
