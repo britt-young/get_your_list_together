caa6b2990d74bc232565dfa238218d3110ed0f61
Maggie McCausland <maggiemccausland@hotmail.com>
2023-11-29 17:49:37 -0700

    added sequelize to connection js and server.js  commented out code

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 
+// Set up Sequelize
+// const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 
 // Set Handlebars as the default templating engine
@@ -21,3 +25,21 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
+
+// Configure and link a session object with the sequelize store
+// const sess = {
+//     cookie: {},
+//     resave: false,
+//     saveUninitialized: true,
+//     store: new SequelizeStore({
+//       db: sequelize
+//     })
+//   };
+  
+  // Add express-session and store as Express.js middleware
+//   app.use(session(sess));
+
+// Sequelize
+// sequelize.sync({ force: false }).then(() => {
+//     app.listen(PORT, () => console.log('Now listening'));
+//   });
