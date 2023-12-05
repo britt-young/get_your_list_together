7cdc664c02a4e7bcb61520fed3d4753eb93d3231
Maggie McCausland <maggiemccausland@hotmail.com>
2023-12-04 19:06:17 -0700

    added messages, added signout functions to handlebars

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const sequelize = require("../../config/connection");
+// const sequelize = require("../../config/connection");
 const firebase = require("@firebase/app");
 require("@firebase/auth");
 const router = express.Router();
